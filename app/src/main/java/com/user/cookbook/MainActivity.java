d0b03d64d584a015c14b279654aff6102d97449b
kurdesz <ka.kwilosz@gmail.com>
2019-11-24 23:58:33 +0100

    still not working

--- app/src/main/java/com/user/cookbook/MainActivity.java
+++ app/src/main/java/com/user/cookbook/MainActivity.java
@@ -1,7 +1,12 @@
 package com.user.cookbook;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.user.cookbook.fragments.MenuFragment;
 
 
 public class MainActivity extends FragmentActivity {
@@ -10,6 +15,13 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Fragment menuPanel = new MenuFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment2, menuPanel);
+        transaction.commit();
 
     }
 }
