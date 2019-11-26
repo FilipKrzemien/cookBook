c7565e36a9b49ec6a4b7cdf22b88ac6ef95d522a
Filip <filip.krzem@gmail.com>
2019-11-26 01:24:12 +0100

    ola merge + fix

--- app/src/main/java/com/user/cookbook/MainActivity.java
+++ app/src/main/java/com/user/cookbook/MainActivity.java
@@ -1,9 +1,13 @@
 package com.user.cookbook;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.user.cookbook.add.recepie.AddRecipe;
 import com.user.cookbook.db.DataBaseHelper;
 import com.user.cookbook.db.dao.IngredientDAO;
 import com.user.cookbook.db.dao.RecipeDAO;
@@ -25,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
         stepDAO = new StepDAO(db);
         ingredientDAO = new IngredientDAO(db);
         recipeDAO = new RecipeDAO(db, stepDAO, ingredientDAO);
+        Button button1 = (Button) findViewById(R.id.button4);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AddRecipe.class);
+                view.getContext().startActivity(intent);}
+        });
     }
 }
